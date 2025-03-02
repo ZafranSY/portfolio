285ca5ca26499f43499656209db937ed65f9421c
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-03 04:32:05 +0800

    Update SocMed.tsx

--- src/app/component/SocMed.tsx
+++ src/app/component/SocMed.tsx
@@ -6,22 +6,30 @@ type cardProps = {
   title: string;
   desc: string;
   icon: IconType;
+  link: string;
 };
-const SocMed: React.FC<cardProps> = ({ title, desc, icon: Icon }) => {
+const SocMed: React.FC<cardProps> = ({ title, desc, icon: Icon, link }) => {
   return (
-    <div className="flex items-center justify-between p-4 bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-300 cursor-pointer">
-      <div className="flex items-center gap-4">
-        <div className="text-2xl">{Icon && <Icon />}</div>
-        <div>
-          <p className="font-semibold text-black">{title}</p>
-          <p className="text-gray-500">{desc}</p>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-between p-4 bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-300 cursor-pointer"
+    >
+      <div className="flex items-center justify-between p-4 bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-300 cursor-pointer">
+        <div className="flex items-center gap-4">
+          <div className="text-2xl">{Icon && <Icon />}</div>
+          <div>
+            <p className="font-semibold text-black">{title}</p>
+            <p className="text-gray-500">{desc}</p>
+          </div>
+        </div>
+
+        <div className="text-gray-400 text-3xl">
+          <MdKeyboardArrowRight />
         </div>
       </div>
-
-      <div className="text-gray-400 text-3xl">
-        <MdKeyboardArrowRight />
-      </div>
-    </div>
+    </a>
   );
 };
 
